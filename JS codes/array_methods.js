edc1261f05db816866cd1e4bcb3f47d55f78ab70
niniefut <niniefut6711@gmail.com>
2025-09-27 19:40:07 +0100

    Added some examples

--- JS codes/array_methods.js
+++ JS codes/array_methods.js
@@ -1,3 +1,16 @@
 let nums=[11,13,15,17,19]
 let cubeNums = nums.map(n=> (n*n*n))
 console.log(cubeNums)
+
+let evenNums = nums.filter(n => n % 2 === 0);
+console.log("Even numbers:", evenNums);
+
+let sum = nums.reduce((acc, n) => acc + n, 0);
+console.log("Sum:", sum);
+
+nums.forEach(n => {
+  console.log("Number:", n);
+});
+
+let firstBig = nums.find(n => n > 15);
+console.log("First number greater than 15:", firstBig);
